7c389ed61c5f47178a701c0d48baf1f40269e72c
Giuseppe Iannello <giuseppe.iannello@brokenloop.net>
2022-10-18 15:19:24 +0200

    Implement `getVersion` command

--- features/support/hardwareInformation.ts
+++ features/support/hardwareInformation.ts
@@ -5,6 +5,10 @@ When('I ask for the stick name', async function () {
     this['response'] = await this['wms'].getName();
 });
 
+When('I ask for the stick version', async function () {
+    this['response'] = await this['wms'].getVersion();
+});
+
 Then('the stick responds with {string}', async function (value) {
     assert.strictEqual(this['response'], value)
 });
--- lib/WaremaWMSFrame.ts
+++ lib/WaremaWMSFrame.ts
@@ -4,3 +4,7 @@ export interface WaremaWMSFrame {
 export interface WaremaWMSFrameName extends WaremaWMSFrame {
     name: string
 }
+
+export interface WaremaWMSFrameVersion extends WaremaWMSFrame {
+    version: string
+}
